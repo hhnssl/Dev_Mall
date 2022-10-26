ab8be3447bc4a69c03c63311593518eed79ddd4c
hhnssl <losnah5h@gmail.com>
2022-10-26 15:26:19 +0900

    초기화 전 백업

--- src/static/js/views/join.js
+++ src/static/js/views/join.js
@@ -1,23 +1,37 @@
 const form = document.getElementById('form');
-// input 요소
-const id = document.getElementById('username');
+const username = document.getElementById('username');
 const password = document.getElementById('password');
-const passwordConfirm = document.getElementById('password2');
+const password2 = document.getElementById('password2');
 const name = document.getElementById('name');
-const phoneNumber1 = document.querySelector('.phone-number1');
-const phoneNumber2 = document.querySelector('.phone-number2');
-const phoneNumber3 = document.querySelector('.phone-number3');
-const email1 = document.querySelector('.email1');
-const email2 = document.querySelector('.email2');
+const phone_number = document.getElementById('phone_number');
 
-id.addEventListener('blur', (e) => {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const formControl = id.parentElement.parentElement;
-  // console.log(id.value);
+// 에러 메세지 띄우기
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
 
-  if (id.value === '') {
-    // console.log('값이 비었네');
-    formControl.classList = error;
-  }
+  const span = formControl.querySelector('span');
+  span.innerText = message;
+}
+
+// 성공
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control';
+}
+
+// 폼 제출버튼 눌렀을 때
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const inputs = [username, password, password2, name, phone_number];
+  // console.log(name.value);
+
+  inputs.forEach((input) => {
+    if (input.value === '') {
+      showError(input, '필수 입력 항목입니다.');
+    } else {
+      showSuccess(input);
+    }
+  });
 });
